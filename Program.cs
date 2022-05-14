d316e4505fcb63f57a55c8df30634eb2c7d9c250
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-05-14 20:48:53 +0100

    Init IsPrimeNumberFunction

--- Program.cs
+++ Program.cs
@@ -11,5 +11,21 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
         }
+
+        public int IsPrimeNumber(int start, int end)
+        {
+
+            Console.WriteLine("This Program prints the prime numbers in a range of numbers.\n" +
+                "Enter a starting number, and an end number");
+            Console.WriteLine("Enter the start number: ");
+            start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the end number: ");
+            end = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Prime Numbers between {start} and {end} are : ");
+            for(int i = start; i <= end; i++)
+            {
+
+            }
+        }
     }
 }
