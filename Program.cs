03c02bee7f2d3eeb1600d5c3ce670650b4e242b3
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-05-14 21:47:18 +0100

    ReverseString

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.IsPrimeNumber();
+            //program.IsPrimeNumber();
+            program.ReverseString();
+        }
+
+        public void ReverseString()
+        {
+            Console.Write("Enter a String : ");
+            string originalString = Console.ReadLine();
+            string reverseString = string.Empty;
+            foreach (char c in originalString)
+            {
+                reverseString = c + reverseString;
+            }
+            Console.Write($"Reverse String is : {reverseString} ");
+            Console.ReadLine();
         }
 
         public void IsPrimeNumber()
