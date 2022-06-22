d6954d1c71974cc477aa65b2ed0a3b1498d6c7fd
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-06-22 22:11:14 +0100

    binary search example

--- Program.cs
+++ Program.cs
@@ -14,14 +14,19 @@ namespace LogicalPrograms
 
         static void Main(string[] args)
         {
-            //DictionaryDataStruc();
+            BinarySearch();
             Console.ReadLine();
         }
 
         public static void BinarySearch()
         {
             int[] intArray = new int[10] { 1, 3, 5, 7, 11, 13, 17, 19, 23, 31 };
-
+            int target = 17;
+            int pos = Array.BinarySearch(intArray, target);
+            if(pos >= 0)
+                Console.WriteLine($"Item {intArray[pos].ToString()} found at position {pos + 1}. ");
+            else
+                Console.WriteLine("Item not found");
         } 
 
         public static void DictionaryDataStruc()
